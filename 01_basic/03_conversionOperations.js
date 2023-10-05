a36ac2d8bda64a52f06bb51f5db755ffdfd0a127
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-10-05 12:34:43 +0000

    primitive and non primitive data types

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,27 @@
+// let score = true
+// letvalueNumber = Number(score) 
+//  console.log(typeof letvalueNumber);
+//  console.log(letvalueNumber)
+ //Nan a number//
+// "33" => 33
+// 33"abc" -> Nan not a number
+//true=> 1 ; false= 0;
+let isLoggedIn = 1
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanIsLoggedIn);
+
+
+
+
+
+
+let someNumber = 33
+
+let stringNumber =  String(someNumber)
+console.log( typeof stringNumber)
+
+
+
+
+
+
