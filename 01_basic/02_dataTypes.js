a9e02d35472dffdaacbad2b48dc630d1aec0f359
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-10-05 05:40:19 +0000

    Converson operations

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+//"use strict"// treat all js code as a newer version
+
+ //readablity should be arise here 
+console.log("alkma")
+
+let name = "alkma"
+let age = 18
+let isLoggedIn = false
+
+// number =>2 tp power 53
+//string => ""
+//bollean => True/false
+// null => standalone value
+// underdefined => 
+//symbol =>unique
+
+//Object
+
+console.log(typeof "Hitesh"); //String
+console.log(typeof undefined);// undefined
+console.log(typeof null) // object
