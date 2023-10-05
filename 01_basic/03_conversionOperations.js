2bd1b6645cf8e142ebbb06b5e20687addc756604
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-10-05 12:35:50 +0000

    primitive and non primtive data types

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -9,19 +9,16 @@
 let isLoggedIn = 1
 let bolleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(bolleanIsLoggedIn);
-
-
-
-
-
-
+//*********************** */operations***********************
+ let num1, num2, num3
+  num1 = num2 = num3 = 2+2
 let someNumber = 33
 
 let stringNumber =  String(someNumber)
-console.log( typeof stringNumber)
+// console.log( typeof stringNumber)
 
-
-
-
+let gamCounter = 100
+gamCounter++
+//console.log(gamCounter);
 
 
---
+++ 01_basic/data_type_summary.js
@@ -0,0 +1,49 @@
+// data types are two types 
+//  Primitive Daata time are callby value
+// 7 types of primitive data types
+//  string : number: Bollean: null: undefined: symbol: BigInt
+
+
+////////////////////*/////////////////////
+
+const scroe = 100
+const scoreValue = 363.3
+
+const isLoggedIn = false
+const outsidTemp = null
+ let udeeEmail; ////undefied value will be arise here]
+
+// ex of symbol
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id===anotherId);
+
+
+const bigNumber = 673764674376467476n // if n will added to the number then it wall called big Int
+
+
+///////////////////*******************////////////////////////// */
+// Non primitive data types; Refrenec non primitive data types
+ // array; obeject; functions
+
+const heros = [ "shakitamn", "naharga",  "shuhgam"] // Array
+ 
+let myObj = {
+
+    name: "hitesh",     //  Objects
+    age: 22,
+    
+ }
+
+//function
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber);
+
+// In Non Primitive data types the type of function always return
+
+// object function
